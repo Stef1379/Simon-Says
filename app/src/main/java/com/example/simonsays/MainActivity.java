29f85cb492d69d380b0e9d9770cd5de481e211fc
Stef1397 <stefv.d.loo@outlook.com>
2024-06-30 21:27:23 +0200

    Add consent

--- app/src/main/java/com/example/simonsays/MainActivity.java
+++ app/src/main/java/com/example/simonsays/MainActivity.java
@@ -7,13 +7,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+import java.util.EnumMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAnalytics firebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setContentView(R.layout.activity_main);
+
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        Map<FirebaseAnalytics.ConsentType, FirebaseAnalytics.ConsentStatus> consentMap = new EnumMap<>(FirebaseAnalytics.ConsentType.class);
+        consentMap.put(FirebaseAnalytics.ConsentType.ANALYTICS_STORAGE, FirebaseAnalytics.ConsentStatus.GRANTED);
+        consentMap.put(FirebaseAnalytics.ConsentType.AD_STORAGE, FirebaseAnalytics.ConsentStatus.GRANTED);
+        consentMap.put(FirebaseAnalytics.ConsentType.AD_USER_DATA, FirebaseAnalytics.ConsentStatus.GRANTED);
+        consentMap.put(FirebaseAnalytics.ConsentType.AD_PERSONALIZATION, FirebaseAnalytics.ConsentStatus.GRANTED);
+
+        firebaseAnalytics.setConsent(consentMap);
+
 
         Button startGame = findViewById(R.id.btn_start);
         startGame.setOnClickListener(v -> {
